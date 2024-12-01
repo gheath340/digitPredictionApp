71632f113f3cc07a85caa5571ef2168abcc7b1c6
Garrett Heath <heathsmail0@gmail.com>
2024-12-01 16:36:47 -0700

    implemented random manipulations to training images to reduce overfitting

--- Backend/data_formatting.py
+++ Backend/data_formatting.py
@@ -18,17 +18,28 @@ def normalize_test_data(data):
 
 #Randomly rotates image between max_angle and -max_angle
 def random_rotation(image, max_angle=15):
+    image = Image.fromarray(image)
     angle = random.uniform(-max_angle, max_angle)
-    return image.rotate(angle)
+    image = image.rotate(angle)
+    return np.array(image)
     
 #Randomly shifts image between max_shift angle and -max_shift angle
 def random_shift(image, max_shift=0.1):
-    width, height = image.shape
+    image = Image.fromarray(image)
+    width, height = image.size
     max_dx = int(max_shift * width)
     max_dy = int(max_shift * height)
     dx = random.randint(-max_dx, max_dx)
     dy = random.randint(-max_dy, max_dy)
-    return ImageOps.offset(image, dx, dy)
+    # Define the affine transformation matrix for shifting
+    transformation_matrix = (1, 0, dx, 0, 1, dy)
+    shifted_image = image.transform(
+        image.size,
+        Image.AFFINE,
+        transformation_matrix,
+        resample=Image.Resampling.NEAREST
+    )
+    return np.array(shifted_image)
     
 #Randomly zooms image between max_zoom and min_zoom
 def random_zoom(image, min_zoom=0.9, max_zoom=1.1):
